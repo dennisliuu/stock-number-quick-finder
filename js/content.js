bbd7c773fbbbcf03bc9ec03f868f812a1f3cd66e
dennisliuu <dennisliuu@gmail.com>
2020-07-16 12:03:38 +0800

    update

--- js/content.js
+++ js/content.js
@@ -16,22 +16,36 @@ function getUserSelection() {
             div.style.bottom = "0"
             div.style.right = "0"
             div.id = "stk_no"
+            var dvi_btn = document.createElement("button")
+            div.appendChild(dvi_btn)
+            dvi_btn.id = "close_btn"
+            dvi_btn.innerText = "X"
+            dvi_btn.style.fontSize = "14px"
+            document.getElementById("close_btn").addEventListener("click", close_btn);
         }
     }
 }
 
+function close_btn() {
+    element = document.querySelectorAll("#stk_no");
+    Array.prototype.forEach.call(element, function (node) {
+        node.parentNode.removeChild(node);
+    })
+    console.log('close')
+}
 window.onload = function () {
-    document.onclick = function (e) {
-        var stk_no = document.querySelectorAll('stk_no');
-        if (e.target.id != 'stk_no') {
-            for (let i = 0; i < stk_no.length; i++) {
-                stk_no[i].style.display = 'none'
-                console.log(stk_no[i]);
-                
-            }
-            console.log('hide');
-        }
-    };
+    // document.onclick = function (e) {
+    // var stk_no = document.querySelectorAll('stk_no');
+    // if (e.target.id != 'stk_no') {
+    //     for (let i = 0; i < stk_no.length; i++) {
+    //         stk_no[i].style.display = 'none'
+    //         console.log(stk_no[i]);
+
+    //     }
+    //     console.log('hide');
+    // }
+
+    // };
 };
 
 document.addEventListener("mouseup", getUserSelection)
